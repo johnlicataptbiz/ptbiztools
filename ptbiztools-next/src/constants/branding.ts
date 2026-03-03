8eacb74d29f5c04695fd53361c0f248eebcb5092
Jack Licata <jack@physicaltherapybiz.com>
2026-03-03 06:46:24 -0600

    Set new PT Biz icon as favicon asset

--- ptbiztools-next/src/constants/branding.ts
+++ ptbiztools-next/src/constants/branding.ts
@@ -2,7 +2,7 @@ export const PTBIZ_LOGO_LIGHT_BG_URL = 'https://22001532.fs1.hubspotusercontent-
 export const PTBIZ_LOGO_DARK_BG_URL = 'https://22001532.fs1.hubspotusercontent-na1.net/hubfs/22001532/Logos/PTBIZ_Logo-Primary_1Color_White_RGB.png'
 export const PTBIZCOACH_LOGIN_LOGO_URL = 'https://22001532.fs1.hubspotusercontent-na1.net/hubfs/22001532/Logos/logfin.png'
 export const PTBIZCOACH_LOGO_URL = 'https://22001532.fs1.hubspotusercontent-na1.net/hubfs/22001532/Logos/log.png'
-export const PTBIZCOACH_FAVICON_URL = 'https://22001532.fs1.hubspotusercontent-na1.net/hubfs/22001532/Logos/fa.png'
+export const PTBIZCOACH_FAVICON_URL = '/favicon.png'
 
 export const SITE_LOGO_URL = PTBIZCOACH_LOGO_URL
 export const LOGIN_LOGO_URL = PTBIZCOACH_LOGO_URL
