4d46329c3afaff674ac25e93254f93d690f73c3f
Jack Licata <jack@physicaltherapybiz.com>
2026-03-18 03:39:05 -0500

    feat: use local image assets for tool badges and main logo
    - add compgrader.png, discallgrader.png, plgrader.png, salesgrader.png to public/
    - add ptbizcoach-logo.jpeg to public/
    - update TOOL_BADGES constants to use local /public paths
    - update PTBIZCOACH_LOGO_URL to use local /ptbizcoach-logo.jpeg
      (SITE_LOGO_URL and LOGIN_LOGO_URL inherit this automatically)

--- ptbiztools-next/src/constants/tool-badges.ts
+++ ptbiztools-next/src/constants/tool-badges.ts
@@ -1,7 +1,6 @@
 export const TOOL_BADGES = {
-  discovery: "https://22001532.fs1.hubspotusercontent-na1.net/hubfs/22001532/JL/disccalc.png",
-  sales: "https://22001532.fs1.hubspotusercontent-na1.net/hubfs/22001532/JL/salescalc.png",
-  pl: "https://22001532.fs1.hubspotusercontent-na1.net/hubfs/22001532/JL/plcalc.png",
-  comp: "https://22001532.fs1.hubspotusercontent-na1.net/hubfs/22001532/JL/compcalc.png",
+  discovery: "/discallgrader.png",
+  sales: "/salesgrader.png",
+  pl: "/plgrader.png",
+  comp: "/compgrader.png",
 } as const;
-
